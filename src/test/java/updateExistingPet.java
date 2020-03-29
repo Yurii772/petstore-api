08e75674bb417d54143bcd3c64a88e31b4c096fa
Yurii772 <yurij772@gmail.com>
2020-03-29 17:08:01 +0300

    Merge remote-tracking branch 'github/master'
    # Conflicts:
    #	src/test/java/updatePet.java

---
+++ src/test/java/updateExistingPet.java
@@ -0,0 +1,35 @@
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class updateExistingPet {
+    @Test
+    public void updateExPet(){
+        given()
+                .baseUri("https://petstore.swagger.io")
+                .contentType("application/json")
+                .body("{\n" +
+                        "  \"id\": 0,\n" +
+                        "  \"category\": {\n" +
+                        "    \"id\": 0,\n" +
+                        "    \"name\": \"string\"\n" +
+                        "  },\n" +
+                        "  \"name\": \"doggie\",\n" +
+                        "  \"photoUrls\": [\n" +
+                        "    \"string\"\n" +
+                        "  ],\n" +
+                        "  \"tags\": [\n" +
+                        "    {\n" +
+                        "      \"id\": 0,\n" +
+                        "      \"name\": \"string\"\n" +
+                        "    }\n" +
+                        "  ],\n" +
+                        "  \"status\": \"available\"\n" +
+                        "}")
+                .when()
+                .put("v2/pet")
+                .then()
+                .log().all()
+                .statusCode(200);
+    }
+}
