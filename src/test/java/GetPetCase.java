17e3d3735f2bb4b9b39b62ab6567d46f26770d9a
Yurii772 <yurij772@gmail.com>
2020-03-29 17:53:28 +0300

    Merge remote-tracking branch 'github/master'
    # Conflicts:
    #	src/test/java/updatePet.java

--- src/test/java/GetPetCase.java
+++ src/test/java/GetPetCase.java
@@ -6,28 +6,15 @@ public class GetPetCase {
 
     @Test
     public void getPetByID() {
-        int id = 0;
+        int id = 5;
         given()
                 .baseUri("https://petstore.swagger.io")
                 .when()
                 .get("/v2/pet/{id}", id)
                 .then()
                 .log().all()
-                .statusCode(404);
-    }
-}
-    public class updatePet {
-
-    @Test
-    public void updatePets() {
-        int id = 1;
-        given()
-                .baseUri("https://petstore.swagger.io")
-                .when()
-                .post("/v2/pet/{id}", id)
-                .then()
-                .log().all()
                 .statusCode(200);
     }
 }
+
 
