65660547f6be06b65341f46fb33242c0cd5ec4c5
Yurii772 <yurij772@gmail.com>
2020-03-29 16:00:19 +0300

    Ticket 1 created first rest test

--- src/test/java/updatePet.java
+++ src/test/java/updatePet.java
@@ -3,16 +3,19 @@ import org.junit.Test;
 import static io.restassured.RestAssured.given;
 
 public class updatePet {
-
-    @Test
-    public void updatePets() {
-        int id = 1;
-        given()
-                .baseUri("https://petstore.swagger.io")
-                .when()
-                .post("/v2/pet/{id}", id)
-                .then()
-                .log().all()
-                .statusCode(200);
+        @Test
+        public void updatePet() {
+            int id = 11;
+            given()
+                    .baseUri("https://petstore.swagger.io")
+                    .param("name", "sharikas")
+                    .param("status", "dead")
+                    .when()
+                    .post("/v2/pet/{id}", id)
+                    .then()
+                    .log().all()
+                    .statusCode(200);
+        }
     }
-}
+
+
