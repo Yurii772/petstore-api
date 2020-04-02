daf4d6415cd5683155f3d3d3a49b4838af1bb6cf
Yurii772 <yurij772@gmail.com>
2020-04-03 00:21:26 +0300

    Merge remote-tracking branch 'github/master'
    # Conflicts:
    #	src/test/java/updatePet.java

--- src/test/java/GetPetCase.java
+++ src/test/java/GetPetCase.java
@@ -1,14 +1,25 @@
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import org.junit.Before;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
 
 public class GetPetCase {
+    @Before
+    public void before () {
+        RequestSpecBuilder spec = new RequestSpecBuilder();
+        spec.setBaseUri("https://petstore.swagger.io/v2");
+        spec.addHeader("ContentType", "application/json");
+        RestAssured.requestSpecification=spec.build();
+    }
 
     @Test
     public void getPetByID() {
-        int id = 5;
+        int id = 112316;
         given()
-                .baseUri("https://petstore.swagger.io/v2")
+                .log()
+                .all()
                 .when()
                 .get("/pet/{id}", id)
                 .then()
@@ -17,54 +28,23 @@ public class GetPetCase {
     }
     @Test
     public void updatePetsCase() {
-        int id = 1;
+        int id = 112316;
         given()
-                .baseUri("https://petstore.swagger.io/v2")
-                .param("name", "sharikas")
-                .param("param2", "dead")
+                .param("name" , "sharikas")
+                .param("status", "sold")
+                .log().all()
                 .when()
                 .post("/pet/{id}", id)
                 .then()
                 .log().all()
                 .statusCode(200);
     }
-    @Test
-    public void addPetCase() {
-        given()
-                .baseUri("https://petstore.swagger.io/v2")
-                .contentType("application/json")
-                .body("{\n" +
-                        "  \"id\": 1,\n" +
-                        "  \"category\": {\n" +
-                        "    \"id\": 0,\n" +
-                        "    \"name\": \"string\"\n" +
-                        "  },\n" +
-                        "  \"name\": \"doggie\",\n" +
-                        "  \"photoUrls\": [\n" +
-                        "    \"string\"\n" +
-                        "  ],\n" +
-                        "  \"tags\": [\n" +
-                        "    {\n" +
-                        "      \"id\": 0,\n" +
-                        "      \"name\": \"string\"\n" +
-                        "    }\n" +
-                        "  ],\n" +
-                        "  \"status\": \"available\"\n" +
-                        "}"
-                )
-                .when()
-                .post("/pet/")
-                .then()
-                .log().all()
-                .statusCode(200);
-    }
+
     @Test
     public void updateExPet(){
         given()
-                .baseUri("https://petstore.swagger.io/v2")
-                .contentType("application/json")
                 .body("{\n" +
-                        "  \"id\": 0,\n" +
+                        "  \"id\": 8,\n" +
                         "  \"category\": {\n" +
                         "    \"id\": 0,\n" +
                         "    \"name\": \"string\"\n" +
@@ -79,8 +59,9 @@ public class GetPetCase {
                         "      \"name\": \"string\"\n" +
                         "    }\n" +
                         "  ],\n" +
-                        "  \"status\": \"available\"\n" +
+                        "  \"status\": \"sold\"\n" +
                         "}")
+                .log().all()
                 .when()
                 .put("/pet")
                 .then()
@@ -88,26 +69,57 @@ public class GetPetCase {
                 .statusCode(200);
     }
     @Test
+    public void addPetCase() {
+        String body = "{\n" +
+                "  \"id\": 454,\n" +
+                "  \"category\": {\n" +
+                "    \"id\": 0,\n" +
+                "    \"name\": \"string\"\n" +
+                "  },\n" +
+                "  \"name\": \"UmaTurman\",\n" +
+                "  \"photoUrls\": [\n" +
+                "    \"string\"\n" +
+                "  ],\n" +
+                "  \"tags\": [\n" +
+                "    {\n" +
+                "      \"id\": 0,\n" +
+                "      \"name\": \"string\"\n" +
+                "    }\n" +
+                "  ],\n" +
+                "  \"status\": \"available\"\n" +
+                "}";
+        given()
+                .contentType("application/x-www-form-urlencoded")
+                .body (body)
+                .log().all()
+                .when()
+                .post("/pet/")
+                .then()
+                .log().all()
+                .statusCode(200);
+    }
+
+
+    @Test
     public void delPet(){
-        int idd=5;
+        int id=5;
         given()
                 .log()
                 .all()
-                .baseUri("https://petstore.swagger.io/v2")
                 .header("api_key", "special-key")
                 .when()
-                .delete("/pet/{idd}", idd)
+                .delete("/pet/{id}", id)
                 .then()
                 .log().all()
                 .statusCode(200);
     }
     @Test
     public void getByStatus (){
+        String status="pending";
         given()
-                .pathParam("status", "pending")
-                .baseUri("https://petstore.swagger.io/v2")
                 .when()
-                .get ("/pet/findByStatus?status{status}")
+                .log().all()
+                .get ("/pet/findByStatus?status{status}", status)
                 .then()
                 .log().all()
                 .statusCode(200);
