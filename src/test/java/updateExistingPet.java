dba7cd83c517415808b787d9d1cf6a77ef892460
Yurii772 <yurij772@gmail.com>
2020-04-12 19:43:15 +0300

    Refactored with separate endpoints class

--- src/test/java/updateExistingPet.java
+++ src/test/java/updateExistingPet.java
@@ -1,29 +1,18 @@
-import io.restassured.RestAssured;
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.http.ContentType;
 import io.restassured.response.ValidatableResponse;
-import org.hamcrest.Matchers;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.is;
-
 public class updateExistingPet {
+    PetEndpoint petEndpoint=new PetEndpoint();
     long addedPetId;
     String updName = "Sharik";
+    String updStatus = "sold";
     @Before
-    public void before2() {
-        RequestSpecBuilder spec = new RequestSpecBuilder();
-        spec.setBaseUri("https://petstore.swagger.io/v2");
-        spec.addHeader("Content-Type", "application/json");
-        RestAssured.requestSpecification = spec.build();
-    }
-    @Before
-    public void before1() {
+    public void createPreconditions() {
+        int id=0;
         String body = "{\n" +
-                "  \"id\":\"0\",\n" +
+                "  \"id\":\""+id+"\",\n" +
                 "  \"category\": {\n" +
                 "    \"id\": 0,\n" +
                 "    \"name\": \"string\"\n" +
@@ -40,30 +29,13 @@ public class updateExistingPet {
                 "  ],\n" +
                 "  \"status\": \"available\"\n" +
                 "}";
-        ValidatableResponse response = given()
-                .body(body)
-                .when()
-                .post("/pet")
-                .then()
-                .log()
-                .all();
+        ValidatableResponse response = petEndpoint.createPet(body);
         addedPetId = response.extract().path("id");
-        System.out.println(addedPetId);
     }
     @After
     public void deleteCreatedPreconditions () {
-        given()
-                .log()
-                .all()
-                .header("api_key", "special-key")
-                .when()
-                .delete("/pet/{addedPetId}", addedPetId)
-                .then()
-                .log()
-                .all()
-                .body("message", Matchers.is(String.valueOf(addedPetId)))
-                .statusCode(200);
-}
+        petEndpoint.deletePet(addedPetId);
+    }
     @Test
     public void updateExPet() {
                 String body = ("{\n" +
@@ -82,18 +54,9 @@ public class updateExistingPet {
                 "      \"name\": \"string\"\n" +
                 "    }\n" +
                 "  ],\n" +
-                "  \"status\": \"available\"\n" +
+                "  \"status\": \""+updStatus+"\"\n" +
                 "}");
-        given()
-                .contentType(ContentType.JSON)
-                .body (body)
-                .log().all()
-                .when()
-                .put("/pet/")
-                .then()
-                .log().all()
-                .body("name", is(updName))
-                .statusCode(200);
+        petEndpoint.updExistingPet(body);
     }
 }
 
