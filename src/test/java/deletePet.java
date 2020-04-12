405e668956434db655f0bcdad0345e6bf3e7ceb2
Yurii772 <yurij772@gmail.com>
2020-04-12 18:29:53 +0300

    Refactored using separate endpoints class

--- src/test/java/deletePet.java
+++ src/test/java/deletePet.java
@@ -1,23 +1,12 @@
-import io.restassured.RestAssured;
-import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.response.ValidatableResponse;
 import org.junit.Before;
 import org.junit.Test;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.is;
-
 public class deletePet {
     long addedPetId;
+    PetEndpoint petEndpoint = new PetEndpoint();
     @Before
-    public void before2() {
-        RequestSpecBuilder spec = new RequestSpecBuilder();
-        spec.setBaseUri("https://petstore.swagger.io/v2");
-        spec.addHeader("Content-Type", "application/json");
-        RestAssured.requestSpecification = spec.build();
-    }
-    @Before
-    public void before1() {
+    public void createPreconditions() {
         String body = "{\n" +
                 "  \"id\":\"0\",\n" +
                 "  \"category\": {\n" +
@@ -36,29 +25,12 @@ public class deletePet {
                 "  ],\n" +
                 "  \"status\": \"available\"\n" +
                 "}";
-        ValidatableResponse response = given()
-                .body(body)
-                .when()
-                .post("/pet")
-                .then()
-                .log()
-                .all();
+        ValidatableResponse response=petEndpoint.createPet(body);
         addedPetId = response.extract().path("id");
-        System.out.println(addedPetId);
     }
     @Test
     public void delPet(){
-        given()
-                .log()
-                .all()
-                .header("api_key", "special-key")
-                .when()
-                .delete("/pet/{delId}", addedPetId)
-                .then()
-                .log().all()
-                .body("message", is(String.valueOf(addedPetId)))
-                .statusCode(200);
+        petEndpoint.deletePet(addedPetId);
     }
-
 }
 
